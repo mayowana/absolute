6f045a1cf6537d4a7cc9aaa65f9d6d8a3dbc15bc
Oluwamayowa George <themayowageorge@gmail.com>
2022-03-27 14:40:44 +0100

    Update

--- packages/absolute-theme/src/components/index.js
+++ packages/absolute-theme/src/components/index.js
@@ -1,48 +1,52 @@
-import React from "react"
-import { connect, Global, css, styled } from "frontity"
-import Link from "@frontity/components/link"
-import Switch from '@frontity/components/switch'
-import List from '../components/list'
-import Post from '../components/post'
-import Page from '../components/page'
+import React from "react";
+import { connect, Global, css, styled } from "frontity";
+import Link from "@frontity/components/link";
+import Switch from "@frontity/components/switch";
+import List from "../components/list";
+import Post from "../components/post";
+import Page from "../components/page";
 
 const Root = ({ state }) => {
-  const data = state.source.get(state.router.link)
+  const data = state.source.get(state.router.link);
   return (
     <>
-      <Global styles={css`
-        * {
-          margin: 0; padding: 0; box-sizing: border-box; background-color: black; color: white;
-        }
-        html {
-          font-family: system-ui, Verdana, Arial, sans-serif;
-        }
-      `} />
+      <Global
+        styles={css`
+          * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            background-color: black;
+            color: white;
+          }
+          html {
+            font-family: system-ui, Verdana, Arial, sans-serif;
+          }
+        `}
+      />
       <Header>
         <h1>Absolute</h1>
       </Header>
       <NavBox>
         <Nav>
-          <Link link='/'>Home</Link>
-          <Link link='/entertainment'>Entertainment</Link>
-          <Link link='/sports'>Sports</Link>
-          <Link link='/sports'>Tech</Link>
-
+          <Link link="/">Home</Link>
+          <Link link="/entertainment">Entertainment</Link>
+          <Link link="/sports">Sports</Link>
+          <Link link="/tech">Tech</Link>
+          <Link link="/trending">Trending</Link>
         </Nav>
       </NavBox>
-
 
       <Switch>
         <List when={data.isArchive} />
         <Post when={data.isPost} />
         <Page when={data.isPage} />
       </Switch>
-
     </>
-  )
-}
+  );
+};
 
-export default connect(Root)
+export default connect(Root);
 
 const Header = styled.header`
   height: 5rem;
@@ -50,16 +54,25 @@ const Header = styled.header`
   align-items: center;
   justify-content: center;
   border-bottom: 1px solid white;
-`
+  font-size: 200%;
+`;
 const NavBox = styled.div`
-  width: 90%; margin: 0 auto; display: flex; justify-content: space-between; align-items: center; height: 4rem; border-bottom: 1px solid white;
-`
+  width: 100%;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 2rem;
+  border-bottom: 1px solid white;
+`;
 
 const Nav = styled.nav`
-  display: flex; 
-
+  display: flex;
+  justify-content: center;
+  width: 90%;
+  margin: 0 auto;
   a {
     text-decoration: none;
     margin-left: 1rem;
   }
-`
+`;
